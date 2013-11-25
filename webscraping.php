a2190fe5163057d43ab6fe71603b386939f903ad
Simon Hardouineau <chais.pas@hotmail.fr>
2013-11-25 17:22:10 +0100

    update webscraping + auteur

--- webscraping.php
+++ webscraping.php
@@ -2,6 +2,7 @@
 
 $html = file_get_contents('http://babordplus.univ-bordeaux.fr/notice.php?q=pour%20les%20nuls&spec_expand=1&sort_define=&sort_order=&rows=&start=1'); //get the html returned from the following url
 $babord_doc = new DOMDocument();
+$babord_doc2 = new DOMDocument();
 $html = preg_replace('#<!--.*?-->#s', '', $html); // HTML (<!--tralala-->)
 $html = preg_replace('#/\*.*?\*/#s', '', $html); // PHP (/*tralala*/)
 $html = preg_replace('#//.*$#m', '', $html); // C (// tralala)
@@ -14,43 +15,51 @@ libxml_use_internal_errors(TRUE); //disable libxml errors
 
 if(!empty($html)){ //if any html is actually returned
 
-  $babord_doc->loadHTML($html);
-  libxml_clear_errors(); //remove errors for yucky html
-  
-  $babord_xpath = new DOMXPath($babord_doc);
-
-  $babord_row = $babord_xpath->query('//h5');
+	$babord_doc->loadHTML($html);
+	libxml_clear_errors(); //remove errors for yucky html
+	$babord_xpath = new DOMXPath($babord_doc);
+	$babord_row = $babord_xpath->query('//h5');
 	
-  if($babord_row->length > 0){
-      foreach($babord_row as $row){
-		echo $row->nodeValue;
-      }
-  }
+	echo "Titre :";
+	
+	if($babord_row->length > 0){
+		foreach($babord_row as $row){
+			echo $row->nodeValue;
+		}
+	}
+	
+	echo "<br>";
+
+	$i=0;  
+	$babord_row = $babord_xpath->query('//dd');
+	
+	echo "Auteur : ";
+	
+	if($babord_row->length > 0){
+		foreach($babord_row as $row){
+			if($i<1){
+				echo str_replace('Â', '', $row->nodeValue);
+				$i++;
+			}
+		}
+	}
+	
+	echo "<br>";
+
+	echo "Type : ";
+	
+	$i = 7;
+	if($babord_row->length > 0){
+		foreach($babord_row as $row){
+			if($i<8){
+				echo $row->nodeValue;
+				$i++;
+			}
+		}
+	}
+
+	
+
 }
-
-$babord_row = $babord_xpath->query('//dd');
-	
-  if($babord_row->length > 0){
-      foreach($babord_row as $row){
-		echo $row->nodeValue;
-      }
-  }
-  
-$babord_row = $babord_xpath->query('//dd[@stat_rebond_auteur]');
-	
-  if($babord_row->length > 0){
-      foreach($babord_row as $row){
-		echo $row->nodeValue;
-      }
-  }
-  
-$babord_row = $babord_xpath->query('//div[@sid-column-content sid-odd]');
-	
-  if($babord_row->length > 0){
-      foreach($babord_row as $row){
-		echo ("image" + $row->nodeValue);	
-      }
-  }
-  
 
 ?>
