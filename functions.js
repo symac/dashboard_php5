962e8b09e49f54be066282e3e6c1761e2a634052
claraRigo <clara.rigaud.c3po@gmail.com>
2014-01-22 14:13:59 +0100

    js

--- functions.js
+++ functions.js
@@ -3,7 +3,30 @@ $(document).ready(function(){
 		$('div.actu').slideUp("slow");
 		$('div.actu').slideDown("slow");
 
-	},5000);
-	
-	
+	},30000);
+
+	setInterval(function(){
+		$('div.actu').slideUp("slow");
+		$('div.actu').slideDown("slow");
+
+	},30000);
+
+
+var datastring = 'objet='+objet+'&nom='+nom+'&email='+email+'&message='+message;
+
+
+$.ajax({
+	type:"POST",
+	url:"getdatas/getInfoLivre.php",
+	data: datastring,
+
+	success: function() {  
+	    $('#formcont').html("<div id='message'></div>");  
+	    $('#message').html("<h3>C'est parti !</h3>")  
+	    .append("<p>Je vous répondrais très prochainement, à bientôt !</p>")  
+	    .hide()  
+	    .fadeIn(1500);  
+	  }  
+});
+
 });
