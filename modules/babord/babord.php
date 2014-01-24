7891153095ce1d2e37355179f90e25488017da04
Jean-Marie Cart-Lamy <contact@cartlamy.com>
2014-01-24 15:18:30 +0100

    Synchro babord

--- modules/babord/babord.php
+++ modules/babord/babord.php
@@ -20,22 +20,22 @@ $livres= getBabord();
 </div>
 <div class="cadre_blanc_babord2">
 	<div class="livre" id="livre1">
-		<div class="visuel1"><img src="<?php echo ($livres[$idlivre+1][2]); ?>" alt="Les héritiers d enkidiev"/></div>
+		<div class="visuel1"><img src="<?php echo ($livres[$idlivre+1][2]); ?>" onError="this.onerror=null;this.src='style/images/unknow_book.jpg';"/></div>
 		<div class="titre1"><?php echo utf8_encode($livres[$idlivre+1][0]); ?></div>
 		<div class="auteur1"><?php echo utf8_encode($livres[$idlivre+1][1]); ?></div>
 	</div>
 	<div class="livre" id="livre2">
-		<div class="visuel2"><img src="<?php echo ($livres[$idlivre+2][2]); ?>" alt="Cinq jours"/></div>
+		<div class="visuel2"><img src="<?php echo ($livres[$idlivre+2][2]); ?>" onError="this.onerror=null;this.src='style/images/unknow_book.jpg';"/></div>
 		<div class="titre2"><?php echo utf8_encode($livres[$idlivre+2][0]); ?></div>
 		<div class="auteur2"><?php echo utf8_encode($livres[$idlivre+2][1]); ?></div>
 	</div>
 	<div class="livre" id="livre3">
-		<div class="visuel3"><img src="<?php echo ($livres[$idlivre+3][2]); ?>" alt="Une envie de vérité"/></div>
+		<div class="visuel3"><img src="<?php echo ($livres[$idlivre+3][2]); ?>" onError="this.onerror=null;this.src='style/images/unknow_book.jpg';"/></div>
 		<div class="titre3"><?php echo utf8_encode($livres[$idlivre+3][0]); ?></div>
 		<div class="auteur3"><?php echo utf8_encode($livres[$idlivre+3][1]); ?></div>
 	</div>
 		<div class="livre" id="livre4">
-		<div class="visuel4"><img src="<?php echo ($livres[$idlivre+4][2]); ?>" alt="Une envie de vérité"/></div>
+		<div class="visuel4"><img src="<?php echo ($livres[$idlivre+4][2]); ?>" onError="this.onerror=null;this.src='style/images/unknow_book.jpg';"/></div>
 		<div class="titre4"><?php echo utf8_encode($livres[$idlivre+4][0]); ?></div>
 		<div class="auteur4"><?php echo utf8_encode($livres[$idlivre+4][1]); ?></div>
 	</div>
